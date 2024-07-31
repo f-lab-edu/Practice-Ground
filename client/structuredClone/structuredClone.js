ada23d2fc346a0faf97dfe5799d93837c9f85f34
kw0n0 <alwayskw0n0@gmail.com>
2024-07-31 16:58:38 +0900

    chore: 접근성을 위해 라우팅 추가

--- client/structuredClone/structuredClone.js
+++ client/structuredClone/structuredClone.js
@@ -1,4 +1,5 @@
 function structuredClonePolyfill(value, options = null) {
+  //TODO: Certain object properties are not preserved에 해당하는 항목들 추가, 직렬화(저장/전송/호환성)되지 않는 경우
   if (typeof value === 'function' || value.nodeType !== undefined) {
     throw new Error('DataCloneError');
   }
